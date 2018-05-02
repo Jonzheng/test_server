6e6e9bc42011e509e8bba4e7662e9588deda5d9b
Jon <jonoz@qq.com>
2018-05-02 22:37:19 +0800

    日常更新

--- server/controllers/qAudio.js
+++ server/controllers/qAudio.js
@@ -2,10 +2,13 @@ const { mysql } = require('../qcloud')
 
 module.exports = async ctx => {
     let body = ctx.request.body
-    var fields = body.fields
-
+    var data = ""
+    if (body.file_id){
+        data = await mysql('t_audio').select('*').where('file_id', body.file_id)
+    } else{
+        data = await mysql('t_audio').select('*')
+    }
     //const data = await mysql('t_audio').select('*').where('level', 1)
     //const data = await mysql('t_audio').select('*').leftJoin('t_list', 't_audio.file_id', 't_list.file_id')
-    const data = await mysql('t_audio').select('*')
     ctx.state.data = data
 }
